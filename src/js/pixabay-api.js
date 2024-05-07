c61fc833c1e0dd0ed9b69bd8245be1d211954dbb
IgorMatvienko <38344025+gorisgor@users.noreply.github.com>
2024-05-07 14:10:32 +0300

    u

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,28 +1,47 @@
 
-export const request = document.querySelector(".request");
-export const button = document.querySelector(".submit-btn");
+const request = document.querySelector(".request");
+const button = document.querySelector(".submit-btn");
 
 const API_KEY = "43757696-1a32682a4096c08080d446579";
-const URL = "https://pixabay.com/api/"
-const searchParams = new URLSearchParams({
-    key: API_KEY,
-    q: "dogs",
-    image_type: "photo",
-    orientation: "horizontal",
-    safesearch: true,
-  });
+const URL = "https://pixabay.com/api/";
 
-export const fetch = fetch(`${URL}${searchParams}`).then(response => {
-    if (!response.ok) {
-      throw new Error(
-        "Sorry, there are no images matching your search query. Please try again!"
-      );
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-})
-.catch(error => {
-    console.error("Error fetching images:", error);
-});;
+function fetchImages() {
+    const searchParams = new URLSearchParams({
+        key: API_KEY,
+        q: request.value,
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: true,
+    });
+
+    fetch(`${URL}?${searchParams}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(
+                    "Sorry, there are no images matching your search query. Please try again!"
+                );
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error("Error fetching images:", error);
+        });
+}
+
+request.addEventListener("input", onRequest);
+let userRequest = ""; 
+
+function onRequest(event) {
+    event.preventDefault();
+    userRequest = request.value;
+}
+
+button.addEventListener("click", onSubmit);
+
+function onSubmit(event) {
+    event.preventDefault();
+    fetchImages();
+}
--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
-import { button, request } from "./js/pixabay-api";
+// import { button, request } from "./js/pixabay-api";
 
 
-request.addEventListener("input", onRequest);
-let userRequest= 0;
-function onRequest(event){
-    event.preventDefault();
-    userRequest = request.value
-}
-button.addEventListener("click", onSubmit)
-  function onSubmit(event) {
-    event.preventDefault();
-    searchParams.q = userRequest;
-    console.log(userRequest);
-    fetch();
-  }
+// request.addEventListener("input", onRequest);
+// let userRequest= 0;
+// function onRequest(event){
+//     event.preventDefault();
+//     userRequest = request.value
+// }
+// button.addEventListener("click", onSubmit)
+//   function onSubmit(event) {
+//     event.preventDefault();
+//     searchParams.q = userRequest;
+//     console.log(userRequest);
+//     fetch();
+//   }
