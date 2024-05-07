9cc1a784ae0675455d9a2a38350453bd665c6ba9
IgorMatvienko <38344025+gorisgor@users.noreply.github.com>
2024-05-07 11:18:08 +0300

    u

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,20 +1,22 @@
-const userRequest= "dogs";
+
+export const request = document.querySelector(".request");
+export const button = document.querySelector(".submit-btn");
+
 const API_KEY = "43757696-1a32682a4096c08080d446579";
 const URL = "https://pixabay.com/api/"
 const searchParams = new URLSearchParams({
     key: API_KEY,
-    q: userRequest,
+    q: "dogs",
     image_type: "photo",
-    orientation: horizontal,
+    orientation: "horizontal",
     safesearch: true,
   });
 
-  fetch(`${URL}${searchParams}`).then(response => {
+export const fetch = fetch(`${URL}${searchParams}`).then(response => {
     if (!response.ok) {
       throw new Error(
         "Sorry, there are no images matching your search query. Please try again!"
       );
     }
-
     return response.json();
   });
