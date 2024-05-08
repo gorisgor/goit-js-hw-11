fc1095efb6bdab784b9c60908b18bbac656881c0
IgorMatvienko <38344025+gorisgor@users.noreply.github.com>
2024-05-08 12:37:43 +0300

    u

--- src/main.js
+++ src/main.js
@@ -1,16 +1,69 @@
-// import { button, request } from "./js/pixabay-api";
+const URL = "https://pixabay.com/api/";
 
+let userRequest = ""; 
+const searchParams = new URLSearchParams({
+    key: "43757696-1a32682a4096c08080d446579",
+    q: userRequest,
+    image_type: "photo",
+    orientation: "horizontal",
+    safesearch: true,
+});
 
-// request.addEventListener("input", onRequest);
-// let userRequest= 0;
-// function onRequest(event){
-//     event.preventDefault();
-//     userRequest = request.value
-// }
-// button.addEventListener("click", onSubmit)
-//   function onSubmit(event) {
-//     event.preventDefault();
-//     searchParams.q = userRequest;
-//     console.log(userRequest);
-//     fetch();
-//   }
+function fetchImages(searchParams) {
+    return fetch(`${URL}?${searchParams}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Не вдалося виконати запит до API Pixabay');
+            }
+            return response.json();
+        })
+        .catch(error => {
+            console.error('Помилка при отриманні зображень:', error);
+            throw error;
+        });
+}
+
+function onSearch(event) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    userRequest = form.elements.query.value;
+    searchParams.set('q', userRequest); 
+    loader.classList.remove('is-hidden');
+   
+    fetchImages(searchParams)
+        .then(renderImageCards) 
+        .catch(onFetchError)
+        .finally(() => {
+            form.reset();
+            loader.classList.add('is-hidden');
+        });
+}
+
+const gallery = document.querySelector('.gallery');
+const searchForm = document.querySelector('.form');
+const loader = document.querySelector('.loader');
+
+searchForm.addEventListener('submit', onSearch);
+
+function renderImageCards(data) {
+    gallery.innerHTML = "";
+    data.hits.forEach(image => {
+        const { webformatURL, tags, likes, views, comments, downloads } = image;
+        const markup = `<div class="card">
+            <div class="card-img-top">
+                <img src="${webformatURL}" alt="${tags}">
+            </div>
+            <div class="card-body">
+                <p class="card-text">Likes: ${likes}</p>
+                <p class="card-text">Views: ${views}</p>
+                <p class="card-text">Comments: ${comments}</p>
+                <p class="card-text">Downloads: ${downloads}</p>
+            </div>
+        </div>`;
+        gallery.insertAdjacentHTML('beforeend', markup);
+    });
+}
+
+function onFetchError(error) {
+    alert(error);
+}
